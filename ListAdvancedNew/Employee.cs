673b14e7eb69427e4d8c3cc6d1a350db8f3ef63a
Zuhair Haroon Khan <zk222ac@gmail.com>
2019-02-18 11:24:05 +0100

    Final Version

---
+++ ListAdvancedNew/Employee.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAdvancedNew
+{
+   public class Employee : IComparer<Employee>
+    {
+        // Get properties values 
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Age { get; set; }
+
+        public SortingEmployes SortingEmployes1;
+
+        public Employee(string name, int salary, int age )
+        {
+            // setting values
+            Name = name;
+            Salary = salary;
+            Age = age;
+            
+        }
+
+        public Employee(SortingEmployes st)
+        {
+            SortingEmployes1 = st;
+        }
+
+        public int Compare(Employee x, Employee y)
+        {
+            int result = 0;
+            SortingEmployes sort = SortingEmployes1;
+            if (sort == SortingEmployes.SortByAge)
+            {
+                if (x != null && y != null)
+                {
+                    result = x.Age.CompareTo(y.Age);
+                }
+            }
+            else if (sort == SortingEmployes.SortByName)
+            {
+                if (x != null && y != null)
+                {
+                    result = String.Compare(x.Name, y.Name, StringComparison.Ordinal);
+                }
+            }
+            else
+            {
+                if (sort == SortingEmployes.SortBySalary)
+                {
+                    if (x != null && y != null)
+                    {
+                        result = x.Salary.CompareTo(y.Salary);
+                    }
+                }
+            }
+
+            return result;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Name:{Name}, Salary: {Salary} , Age: {Age}";
+        }
+
+       public enum SortingEmployes
+        {
+            SortByName,
+            SortBySalary,
+            SortByAge
+
+        }
+    }
+}
--- ListAdvancedNew/Program.cs
+++ ListAdvancedNew/Program.cs
@@ -10,12 +10,61 @@ namespace ListAdvancedNew
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee("John Doe" , 5000 , 66 ),
+                new Employee("Jane smith" , 7000 , 22 ),
+                new Employee("Nick slick" , 8000 , 11),
+                new Employee("Mildred Mintz" , 9000 ,44)
+            };
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"Name:{emp.Name}, Salary:{emp.Salary},Age:{emp.Age}");
+            }
+
+            Console.WriteLine(" After Sorting by Name ...................");
+            
+            employees.Sort(new Employee(Employee.SortingEmployes.SortByName));
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"Name:{emp.Name}, Salary:{emp.Salary},Age:{emp.Age}");
+            }
+            Console.WriteLine(" After Sorting by Age ...................");
+
+            employees.Sort(new Employee(Employee.SortingEmployes.SortByAge));
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"Name:{emp.Name}, Salary:{emp.Salary},Age:{emp.Age}");
+            }
+            Console.WriteLine(" After Sorting by Salary  ...................");
+
+            employees.Sort(new Employee(Employee.SortingEmployes.SortBySalary));
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"Name:{emp.Name}, Salary:{emp.Salary},Age:{emp.Age}");
+            }
+            //// retrieve only One object
+            //Employee empStartWithJ = employees.Find(x => x.Name.StartsWith("J"));
+            //Console.WriteLine($"Display the Name whose match or start with  J : {empStartWithJ.Name}");
+
+            //// retrieve multiple object
+            //Console.WriteLine($"Display the Name of all employees whose name is match or start with  J : {empStartWithJ.Name}");
+
+            //var allEmpStartWithJ = employees.FindAll(x => x.Name.StartsWith("J"));
+
+            //foreach (var iEmployee in allEmpStartWithJ)
+            //{
+            //    Console.WriteLine(iEmployee);
+            //}
+
+
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            
         }
     }
 }
